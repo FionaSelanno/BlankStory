581fd40edac0f2e8da6a9fa3f749ae989ecdc71e
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-10-30 17:05:42 +0100

    story + store worde

---
+++ story.js
@@ -0,0 +1,13 @@
+// The missing words:
+var noun = ["cat", "friend", "lollypop", "lady"]
+var verb = ["jumped", "chased",  ]
+var adjective = ["pink", "grumpy"]
+
+// The story:
+console.log("While hanging out after school, I saw a "
++ noun[0] + " perched in a tree. My "
++ noun[1] + " said we should try and catch it. I quickly grabbed my "
++ adjective[0] + " " + noun[2] + " from my backpack. The "
++ noun[0] + " " + verb[0] + " out of the tree. My friend an I "
++ verb[1] + " it passing an"
++ adjective[1] + " " + noun[3] + ".")
