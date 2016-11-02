3b8ce12bbd11a731e22022c32a811a2483035902
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-02 09:31:32 +0100

    randomize the missing words

--- story.js
+++ story.js
@@ -1,13 +1,28 @@
 // The missing words:
-var noun = ["cat", "friend", "lollypop", "lady"]
-var verb = ["jumped", "chased",  ]
-var adjective = ["pink", "grumpy"]
+var noun = ["cat", "friend", "lollypop", "lady"];
+var verb = ["jumped", "chased"];
+var adjective = ["pink", "grumpy"];
+
+
+// Math.random(); will give a random number between 0 and 1, also a decimal number
+// Math.random() * 10; will give a number between 0 and 10
+// Math.floor(); will take a decimal number and round down the nearest whole number.
+// Math.floor(Math.random()*10); will give a round number between 0 and 9, because Math.floor() rounds down the number.
+// Math.floor(Math.random()*10)+1); will give a round number between 1 and 10.
+var rand = function(max) {
+  return(Math.floor(Math.random()* max) + 1);
+};
+
+// -1 to get the index number
+var indexNoun = rand(noun.length)-1;
+var indexAdjective = rand(adjective.length)-1;
+var indexVerb = rand(verb.length)-1;
 
 // The story:
 console.log("While hanging out after school, I saw a "
-+ noun[0] + " perched in a tree. My "
-+ noun[1] + " said we should try and catch it. I quickly grabbed my "
-+ adjective[0] + " " + noun[2] + " from my backpack. The "
-+ noun[0] + " " + verb[0] + " out of the tree. My friend an I "
-+ verb[1] + " it passing an"
-+ adjective[1] + " " + noun[3] + ".")
++ noun[indexNoun] + " perched in a tree. My "
++ noun[indexNoun] + " said we should try and catch it. I quickly grabbed my "
++ adjective[indexAdjective] + " " + noun[indexNoun] + " from my backpack. The "
++ noun[indexNoun] + " " + verb[indexVerb] + " out of the tree. My friend an I "
++ verb[indexVerb] + " it passing an"
++ adjective[indexAdjective] + " " + noun[indexNoun] + ".")
