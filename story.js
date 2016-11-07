0750f00f2836860ce9d8f0fd813ea1fbdc93c444
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-07 10:46:39 +0100

    use the index to get the item of the array

--- story.js
+++ story.js
@@ -6,7 +6,7 @@ var adjectiveArray = ["pink", "grumpy"];
 
 //to prevent the words being repeated I have to pop the used word from the array. And then run the randomize function again:
 
-//step1: create function for getting a random index of any array
+//step 1: create function for getting a random index of any array
 function arrayIndex(items){
   var array = items;
   var lengthArray = array.length;
@@ -16,14 +16,14 @@ function arrayIndex(items){
     var index = number - 1;
     return index
   };
-  return rand(lengthArray)
+  // step 2: use that index to get the item from the array:
+  return array[rand(lengthArray)]
 }
 
 console.log(arrayIndex(nounArray));
 
-// step 2: create function to use that index to get the item from the array
+
 // step 3: function go delete that item form the array
-// console.log(arrayIndex(nounArray));
 
 
 //   var arrayPop = function(index){
@@ -33,10 +33,10 @@ console.log(arrayIndex(nounArray));
 // };
 
 // The story:
-console.log("While hanging out after school, I saw a "
-+ arrayIndex(nounArray) + " perched in a tree. My "
-+ arrayIndex(nounArray) + " said we should try and catch it. I quickly grabbed my "
-+ arrayIndex(adjectiveArray) + " " + arrayIndex(nounArray) + " from my backpack. The "
-+ arrayIndex(nounArray) + " " + arrayIndex(verbArray) + " out of the tree. My friend an I "
-+ arrayIndex(verbArray) + " it passing an"
-+ arrayIndex(adjectiveArray) + " " + arrayIndex(verbArray) + ".")
+// console.log("While hanging out after school, I saw a "
+// + arrayIndex(nounArray) + " perched in a tree. My "
+// + arrayIndex(nounArray) + " said we should try and catch it. I quickly grabbed my "
+// + arrayIndex(adjectiveArray) + " " + arrayIndex(nounArray) + " from my backpack. The "
+// + arrayIndex(nounArray) + " " + arrayIndex(verbArray) + " out of the tree. My friend an I "
+// + arrayIndex(verbArray) + " it passing an"
+// + arrayIndex(adjectiveArray) + " " + arrayIndex(verbArray) + ".")
