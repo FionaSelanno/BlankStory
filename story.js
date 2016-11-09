a587520d1fe3eaf1c9fbbdbb8468dd6c648920a9
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-11-09 17:39:21 +0100

    using html and css to prompt the story

--- story.js
+++ story.js
@@ -1,5 +1,7 @@
+
+
 // The missing words:
-var nounArray = ["cat", "friend", "lollypop"];
+var nounArray = ["cat", "friend", "lollypop", "lady", "house"];
 var verbArray = ["jumped", "chased"];
 var adjectiveArray = ["pink", "grumpy"];
 
@@ -37,13 +39,26 @@ verbArray = verbValues[1];
 var adjectiveValues = arrayItem(adjectiveArray);
 adjectiveArray = adjectiveValues[1];
 
+document.getElementById("noun1").innerHTML = nounValues[0];
+document.getElementById("noun2").innerHTML = arrayItem(nounArray)[0];
+document.getElementById("noun3").innerHTML = arrayItem(nounArray)[0];
+document.getElementById("noun4").innerHTML = arrayItem(nounArray)[0];
+document.getElementById("noun5").innerHTML = arrayItem(nounArray)[0];
+
+document.getElementById("verb1").innerHTML = verbValues[0];
+document.getElementById("verb2").innerHTML = arrayItem(verbArray)[0];
+
+document.getElementById("adjective1").innerHTML = adjectiveValues[0];
+document.getElementById("adjective2").innerHTML = arrayItem(adjectiveArray)[0];
+
 // arrayItem(nounArray)[0] will change the array, so first time calling an item of an array using e.g. nounValues[0].
 // Calling the rest of the item using e.g. arrayItem(nounArray)[0] so that the function with the new array will run again.
 // The story:
-console.log("While hanging out after school, I saw a -"
-+ nounValues[0] + " perched in a tree. My -"
-+ arrayItem(nounArray)[0] + " said we should try and catch it. I quickly grabbed my -"
-+ adjectiveValues[0] + " -" + arrayItem(nounArray)[0] + " from my backpack. The -"
-+ arrayItem(nounArray)[0] + " -" + verbValues[0] + " out of the tree. My friend an I -"
-+ arrayItem(verbArray)[0] + " it passing a -"
-+ arrayItem(adjectiveArray)[0] + " -" + arrayItem(nounArray)[0] + ".")
+
+// console.log("While hanging out after school, I saw a -"
+// + nounValues[0] + " perched in a tree. My -"
+// + arrayItem(nounArray)[0] + " said we should try and catch it. I quickly grabbed my -"
+// + adjectiveValues[0] + " -" + arrayItem(nounArray)[0] + " from my backpack. The -"
+// + arrayItem(nounArray)[0] + " -" + verbValues[0] + " out of the tree. My friend an I -"
+// + arrayItem(verbArray)[0] + " it passing a -"
+// + arrayItem(adjectiveArray)[0] + " -" + arrayItem(nounArray)[0] + ".")
